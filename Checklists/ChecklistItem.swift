10c6127b0726b5c82beed293ce14ef7a7afadab0
Brang Mai <brangmai100@gmail.com>
2021-12-24 19:40:09 -0500

    Add the frame of the Checklists-App

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Brang Mai on 12/24/21.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
