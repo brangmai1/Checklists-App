6fe313d83bdd5b3e75469002f5d175df54c01a70
Brang Mai <brangmai100@gmail.com>
2021-12-26 00:46:59 -0500

    Add the add new list functionalities to the project

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -13,7 +13,7 @@ class ChecklistViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+                
         let item1 = ChecklistItem()
         item1.text = "Walk the dog"
         items.append(item1)
@@ -79,6 +79,20 @@ class ChecklistViewController: UITableViewController {
     func configureText(for cell: UITableViewCell, with item: ChecklistItem) {
         let label = cell.viewWithTag(1000) as! UILabel
         label.text = item.text
+        
+    }
+    
+    // MARK: -Actions
+    @IBAction func  addItem() {
+        let newRowIndex = items.count
+        
+        let item = ChecklistItem()
+        item.text = "I am a new row"
+        items.append(item)
+        
+        let indexPath = IndexPath(row: newRowIndex, section: 0)
+        let indexPaths = [indexPath]
+        tableView.insertRows(at: indexPaths, with: .automatic)
         
     }
 
