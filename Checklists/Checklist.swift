41a089ca07dfcb9bb29fc8a26b1af2a954bc328c
Brang Mai <brangmai100@gmail.com>
2022-01-09 22:49:35 -0500

    Add Checklists array and display it on screen

---
+++ Checklists/Checklist.swift
@@ -0,0 +1,18 @@
+//
+//  Checklist.swift
+//  Checklists
+//
+//  Created by Brang Mai on 1/9/22.
+//
+
+import UIKit
+
+class Checklist: NSObject {
+    var name = ""
+    
+    init(name: String) {
+        self.name = name
+        super.init()
+    }
+
+}
